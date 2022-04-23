8dd7431afe134f51ae6dc89c16b75f09f500263c
Yash Khandelwal <kyash3079@gmail.com>
2022-04-23 21:19:18 +0530

    added console logs in API file

--- src/news-api/NewsAPI.js
+++ src/news-api/NewsAPI.js
@@ -5,6 +5,7 @@ export const TopHeadlines = (country, category, page) => {
     try {
         return `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&page=${page}&apiKey=${apiKey}`;
     } catch (error) {
+        console.log(error);
         return error;
     }
     // else
@@ -15,6 +16,7 @@ export const SearchByInput = (input, page) => {
     try {
         return `https://newsapi.org/v2/everything?q=${input}&page=${page}sortBy=publishedAt&apiKey=3008c96d427e4e58b1311f50c6b60d72`;
     } catch (error) {
+        console.log(error);
         return error;
     }
 }
